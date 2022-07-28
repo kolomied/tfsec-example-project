4154e20d146d107056e5b67bab965e75601d8f50
Dmitry Kolomiets <55797411+kolomied@users.noreply.github.com>
2022-07-28 12:03:47 +0100

    Add another bucket

--- modules/acme_bucket/main.tf
+++ modules/acme_bucket/main.tf
@@ -9,3 +9,15 @@ resource "aws_s3_bucket" "acme_bucket" {
     }
   }
 }
+
+resource "aws_s3_bucket" "second_acme_bucket" {
+  bucket = var.bucket_name
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
