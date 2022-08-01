c8a19dd6bc9ff410e2e30fe07736ba5cf83d79fc
Dmitry Kolomiets <55797411+kolomied@users.noreply.github.com>
2022-08-01 14:01:02 +0100

    Trigger new warning

--- modules/acme_bucket/main.tf
+++ modules/acme_bucket/main.tf
@@ -10,7 +10,19 @@ resource "aws_s3_bucket" "acme_bucket" {
   }
 }
 
-resource "aws_s3_bucket" "second_acme_bucket" {
+  resource "aws_s3_bucket" "second_acme_bucket" {
+    bucket = var.bucket_name
+
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+  }
+
+resource "aws_s3_bucket" "third_acme_bucket" {
   bucket = var.bucket_name
 
   server_side_encryption_configuration {
